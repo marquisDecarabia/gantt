a8a78b0c23092ad159d9a3db75a59753779bdd56
Daria Kriukova <m.decarabia@gmail.com>
2021-03-30 22:54:48 +0300

    feat: readonly mode

--- src/index.js
+++ src/index.js
@@ -86,7 +86,8 @@ export default class Gantt {
             date_format: 'YYYY-MM-DD',
             popup_trigger: 'click',
             custom_popup_html: null,
-            language: 'en'
+            language: 'en',
+            readonly: false
         };
         this.options = Object.assign({}, default_options, options);
     }
